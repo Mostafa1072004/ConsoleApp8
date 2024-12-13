b6f25c4ede228edd7f33a0ab1eef36cc37ce20d6
hp <hp@DESKTOP-RJMTS7Q>
2024-12-13 02:45:02 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    
+    static void Main()
+    {
+
+        #region 1. Write a program that allows the user to enter a number then print it
+        //string input = Console.ReadLine();
+        //double.TryParse(input, out double number);
+        //Console.WriteLine(number); 
+        #endregion
+
+        #region 2. Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+        //int number1 = 10;
+        //int number2 = number1;
+        //number1 = 20;
+        //Console.WriteLine(number1);
+        //Console.WriteLine(number2); 
+        #endregion
+
+        #region 3. Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+        //Person person1 = new Person { Name = "Mohamed" };
+        //Person person2 = person1;
+        //person2.Name = "Mostafa";
+        //Console.WriteLine($"person1.Name: {person1.Name}");
+        //Console.WriteLine($"person2.Name: {person2.Name}"); 
+        #endregion
+    }
+}
